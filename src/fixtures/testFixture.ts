955ad31093311e7839e63e37f1fa5f9913a04856
Prasanth-P29 <prasanth477361@gmail.com>
2026-02-10 18:52:10 +0530

    remove unused test fixture file

--- src/fixtures/testFixture.ts
+++
@@ -1,8 +0,0 @@
-// src/fixtures/testFixture.ts
-import { test as base } from "@playwright/test";
-
-export const test = base.extend({
-  // Logged-in fixture will be added later
-});
-
-export const expect = base.expect;
